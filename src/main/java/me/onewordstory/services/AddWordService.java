3cef496ff1d224e5f0bdbce35594a1d9a25cb4dc
Saketram Durbha <durbhasaketram@gmail.com>
2015-07-13 11:29:54 -0600

    Added initial backend for adding words

---
+++ src/main/java/me/onewordstory/services/AddWordService.java
@@ -0,0 +1,59 @@
+package me.onewordstory.services;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import me.onewordstory.util.Database;
+
+@Path("/words")
+public class AddWordService {
+	
+	@Context 
+	private HttpServletRequest req;
+	
+	@Context
+	private HttpServletResponse resp;
+	
+	@Path("/add")
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Integer addWord(@FormParam(value = "") String word) throws IOException
+	{
+		Database data = Database.getInstance();
+		Connection conn = data.getConnection();
+		int rows = 0;
+		
+		String sql = "INSERT INTO words (word) VALUES (?);";
+		
+		if (word.equals(""))
+		{
+			
+		}
+		
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, word);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rows;
+		
+	}
+
+	
+}
